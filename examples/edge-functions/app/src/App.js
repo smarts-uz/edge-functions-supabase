c8cfa32aad5a392606fedaa4a54af717d8369b08
frontdevjs <front_dev_js@mail.ru>
2023-09-18 16:00:16 +0500

    browser with cors

--- examples/edge-functions/app/src/App.js
+++ examples/edge-functions/app/src/App.js
@@ -5,7 +5,7 @@ import locale from 'react-json-editor-ajrm/locale/en'
 import { supabase } from './utils/supabaseClient'
 import { functionsList } from './functionsList'
 
-const sampleObject = { name: 'world' }
+const sampleObject = { name: 'Supabase!!!' }
 
 function App() {
   const { user } = Auth.useUser()
--- examples/edge-functions/supabase/functions/browser-with-cors/index.ts
+++ examples/edge-functions/supabase/functions/browser-with-cors/index.ts
@@ -2,7 +2,7 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-import { serve } from 'std/server'
+import { serve } from "std/server"
 import { corsHeaders } from '../_shared/cors.ts'
 
 console.log(`Function "browser-with-cors" up and running!`)
