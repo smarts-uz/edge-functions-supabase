02d486fd72dc3b841cd0c905a2044f96f6db63d9
frontdevjs <front_dev_js@mail.ru>
2023-09-25 14:19:31 +0500

    telegram-bot initial start

--- examples/edge-functions/supabase/functions/telegram-bot/index.ts
+++ examples/edge-functions/supabase/functions/telegram-bot/index.ts
@@ -2,28 +2,30 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-import { serve } from 'std/server'
+import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
-console.log(`Function "telegram-bot" up and running!`)
+console.log(`Function "telegram-bot" up and running!`);
 
-import { Bot, webhookCallback } from 'grammy'
+import { Bot, webhookCallback } from "https://deno.land/x/grammy@v1.8.3/mod.ts";
 
-const bot = new Bot(Deno.env.get('TELEGRAM_BOT_TOKEN') || '')
+const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
 
-bot.command('start', (ctx) => ctx.reply('Welcome! Up and running.'))
+bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
-bot.command('ping', (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`))
+bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
+bot.command("hello", (ctx)=>ctx.reply('Welcome! How are you user?'))
 
-const handleUpdate = webhookCallback(bot, 'std/http')
+const handleUpdate = webhookCallback(bot, "std/http");
 
 serve(async (req) => {
-  try {
-    const url = new URL(req.url)
-    if (url.searchParams.get('secret') !== Deno.env.get('FUNCTION_SECRET'))
-      return new Response('not allowed', { status: 405 })
+    try {
+        const url = new URL(req.url);
+        if (url.searchParams.get("secret") !== Deno.env.get("FUNCTION_SECRET")) {
+            return new Response("not allowed", { status: 405 });
+        }
 
-    return await handleUpdate(req)
-  } catch (err) {
-    console.error(err)
-  }
-})
+        return await handleUpdate(req);
+    } catch (err) {
+        console.error(err);
+    }
+});
