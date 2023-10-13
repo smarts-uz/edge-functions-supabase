4b2451a86cb47cd2a7185270ef5c4d166440a747
frontdevjs <front_dev_js@mail.ru>
2023-10-13 16:36:23 +0500

    puppeteer

--- examples/edge-functions/supabase/functions/puppeteer/index.ts
+++ examples/edge-functions/supabase/functions/puppeteer/index.ts
@@ -1,18 +1,19 @@
-import { serve } from 'std/server'
-import puppeteer from 'puppeteer'
+// import { serve } from 'std/server'
+// import puppeteer from 'puppeteer'
 
+import { serve } from 'https://deno.land/std@0.177.0/http/server.ts'
+import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts"
+// import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts"
 serve(async (req) => {
   try {
-    console.log(`wss://chrome.browserless.io?token=${Deno.env.get('PUPPETEER_BROWSERLESS_IO_KEY')}`)
+
     // Visit browserless.io to get your free API token
     const browser = await puppeteer.connect({
-      browserWSEndpoint: `wss://chrome.browserless.io?token=${Deno.env.get(
-        'PUPPETEER_BROWSERLESS_IO_KEY'
-      )}`,
+      browserWSEndpoint: 'wss://chrome.browserless.io?token=a67a4440-1a56-4f0b-b56b-0092534022ef'
     })
     const page = await browser.newPage()
 
-    const url = new URL(req.url).searchParams.get('url') || 'http://www.example.com'
+    const url = new URL(req.url).searchParams.get('url') || 'https://www.google.com'
 
     await page.goto(url)
     const screenshot = await page.screenshot()
